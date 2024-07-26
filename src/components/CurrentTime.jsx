1fd6361dc63354f7b420150327ff1d738428ede9
Tayyab-103 <tayyabhameed103@gmail.com>
2024-07-26 23:33:08 +0500

    Update styling

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -11,12 +11,12 @@ const CurrentTime = () => {
       return () => clearInterval(intervalId);
     }, []);
   return (
-    <div className="container mt-5">
+    <div className="container mt-5" style={{borderRadius:"10px", backgroundColor:"black"}}>
     <div className="card">
-      <div className="card-body text-center">
-        <h1 className="card-title">Current Date and Time</h1>
+      <div className="card-body text-center "  style={{borderRadius:"10px", backgroundColor:"#89FBD4"}}>
+        <h1 className="card-title" style={{fontWeight:"600"}}>Current Date and Time</h1>
         <p className="card-text">{currentDateTime.toLocaleDateString()}</p>
-        <p className="card-text">{currentDateTime.toLocaleTimeString()}</p>
+        <p className="card-text"  style={{borderRadius:"10px", backgroundColor:"white", fontSize:"100px", fontWeight:"700"}}>{currentDateTime.toLocaleTimeString()}</p>
       </div>
     </div>
   </div>
